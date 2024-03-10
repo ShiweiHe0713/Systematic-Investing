5d006f6222e18d5847f079d325c866930e6d1aaf
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-03-09 20:53:03 -0500

    Common update

---
+++ 3_counter_trend_systems/counter_trend.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib
+import seaborn as sbn
+import openpyxl
+
+# Acess sheet 1, set 2nd as the header for dataframe
+file = 'COUNTER_TREND_DATA.xlsx'
+df = pd.read_excel(file, sheet_name=0, header=1)
+
+print(df.head())
